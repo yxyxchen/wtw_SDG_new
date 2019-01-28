f646b559900e78c968ac1423a832ec6fe1f2b60e
yxyxchen <yxyxchen@bu.edu>
2019-01-28 17:23:20 -0500

    add potential reduction scale to simulationFxs.R

--- expParaAnalysis.R
+++ expParaAnalysis.R
@@ -1,159 +1,80 @@
-n = 120
-modelName = "monteRP"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phiR" = vector(length = n), "phiP" = vector(length = n),
-                         "tau" = vector(length = n), "gamma" = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
-summaryPara$optimism = summaryPara$phiR - summaryPara$phiP
-  
-library(ggplot2)
-ggplot(summaryPara, aes(phiR, phiP)) + geom_point() + xlim(c(0,0.2)) +
-  ylim(c(0, 0.2)) + geom_abline(slope = 1, yintercept = 0)
-
-hist(summaryPara$optimism)
-
-### relation ship between AUC and 
+library("ggplot2")
+source("subFxs/plotThemes.R")
 load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(optimism, AUC)) + geom_point() + facet_grid(~condition)
-cor.test(plotData$optimism, plotData$AUC)
 
+loadExpPara = function(modelName, pars){
+  n = 120
+  expPara = matrix(NA, n, (length(pars) + 1))
+  for(sIdx in 1 : n){
+    fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
+    junk = read.csv(fileName)
+    expPara[sIdx, ] = apply(junk , MARGIN = 2, mean)
+  }
+  expPara = data.frame(expPara)
+  colnames(expPara) = c(pars, "LL_all")
+  expPara$id = unique(blockData$id)
+  return(expPara)
+}
 
+plotParaAUC = function(expPara, paraName, blockData){
+  rhoHP = vector(length = 3)
+  rhoLP = vector(length = 3)
+  pHP = vector(length = 3)
+  pLP = vector(length = 3)
+  for(i in 1 : 3){
+    plotData = cbind(expPara, blockData[blockData$blockNum == i, ])
+    corHP = cor.test(plotData[plotData$condition == "HP", paraName], plotData[plotData$condition == "HP",]$AUC, method = "spearman")
+    corLP = cor.test(plotData[plotData$condition == "LP", paraName], plotData[plotData$condition == "LP",]$AUC, method = "spearman")
+    rhoHP[i] = round(corHP$estimate, 3)
+    rhoLP[i] = round(corLP$estimate, 3)
+    pHP[i] = round(corHP$p.value, 3)
+    pLP[i] = round(corLP$p.value, 3)
+  }
+  textData = data.frame(label = c(paste0(rhoHP, "(p =", pHP, ")"), paste0(rhoLP, "(p =", pLP, ")")),
+                        condition = rep(c("HP", "LP"), each = 3),
+                        blockNum = rep(1:3, 2))
+  plotData = left_join(expPara, blockData, by = "id")
+  p = ggplot(plotData, aes_string(paraName, "AUC")) + geom_point() + facet_grid(condition ~ blockNum)  + geom_text(
+      data    = textData,
+      mapping = aes(x = -Inf, y = -Inf, label = label),
+      hjust   = -0.5,
+      vjust   = -3,
+      color = "blue",
+      size = 5
+    )
+  print(p)
+} 
+
+####### monte
+expPara = loadExpPara("monte")
+plotParaAUC(expPara, "phi", blockData)
+plotParaAUC(expPara, "tau", blockData)
+plotParaAUC(expPara, "gamma", blockData)
+#####
+
+expPara = loadExpPara("monteRP", c("phiR", "phiP", "tau", "gamma")) # numPara and nPara
+plotParaAUC(expPara, "tau", blockData)
+plotParaAUC(expPara, "gamma", blockData)
+expPara$optimism = expPara$phiR - expPara$phiP
+plotParaAUC(expPara, "optimism", blockData)
+plotParaAUC(expPara, "phiR", blockData)
+plotParaAUC(expPara, "phiP", blockData)
 ##################
-n = 120
-modelName = "monteBias"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phi" = vector(length = n), 
-                         "tau" = vector(length = n), "gamma" = vector(length = n), "bias" = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
+# monteBias
+expPara = loadExpPara("monteBias", c("phi", "tau", "gamma", "quitBias")) # numPara and nPara
+plotParaAUC(expPara, "tau", blockData)
+plotParaAUC(expPara, "gamma", blockData)
+plotParaAUC(expPara, "phi", blockData)
+plotParaAUC(expPara, "quitBias", blockData)
 
-hist(summaryPara$bias)
-
-### relation ship between AUC and 
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(bias, AUC)) + geom_point() + facet_grid(~condition)
+###################
+#monteSteep
+expPara = loadExpPara("monteSteep", c("phi", "tau", "gamma", "steep")) # numPara and nPara
+plotParaAUC(expPara, "tau", blockData)
+plotParaAUC(expPara, "gamma", blockData)
+plotParaAUC(expPara, "phi", blockData)
+plotParaAUC(expPara, "steep", blockData)
 
 
-###############################
-n = 120
-modelName = "monteRatio"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phi" = vector(length = n), 
-                         "tau" = vector(length = n), "gamma" = vector(length = n), "ratio" = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
-
-hist(summaryPara$ratio)
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(ratio, AUC)) + geom_point() + facet_grid(~condition)
 
 
-###############################
-n = 120
-modelName = "monteSteep"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phi" = vector(length = n), 
-                         "tau" = vector(length = n), "gamma" = vector(length = n), "steep" = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
-
-hist(summaryPara$steep)
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum ==1, ])
-ggplot(plotData, aes(steep, AUC)) + geom_point() + facet_grid(~condition)
-
-#################
-n = 120 
-modelName = "monteRPBias"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phiR" = vector(length = n), "phiP" = vector(length = n), 
-                         "tau" = vector(length = n), "gamma" = vector(length = n), "quitBias"  = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
-summaryPara$optimism = summaryPara$phiR - summaryPara$phiP
-# bias 
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(quitBias, AUC)) + geom_point() + facet_grid(~condition)
-
-# optimusim 
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(optimism, AUC)) + geom_point() + facet_grid(~condition)
-
-
-########################
-n = 120
-modelName = "monteRPBiasSteep"
-samplePara = vector("list", length = n)
-summaryPara = data.frame("phiR" = vector(length = n), "phiP" = vector(length = n), 
-                         "tau" = vector(length = n), "gamma" = vector(length = n), "quitBias"  = vector(length = n),
-                         "steep" = vector(length = n),
-                         "LL_all" = vector(length = n))
-for(sIdx in 1 : n){
-  fileName = sprintf("genData/expModelFitting/%s/s%d.txt", modelName, sIdx)
-  samplePara[[sIdx]] = read.csv(fileName)
-  summaryPara[sIdx, ] = apply(samplePara[[sIdx]] , MARGIN = 2, mean)
-}
-summaryPara$optimism = summaryPara$phiR - summaryPara$phiP
-hist(summaryPara$optimism)
-
-
-# bias 
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(quitBias, AUC)) + geom_point() + facet_grid(~condition)
-
-# optimusim 
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(optimism, AUC)) + geom_point() + facet_grid(~condition)
-
-# steep
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(steep, wtwEarly)) + geom_point() + facet_grid(~condition)
-
-# gamma
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(gamma, AUC)) + geom_point() + facet_grid(~condition)
-
-
-# tau
-library("ggplot2")
-load("genData/expDataAnalysis/blockData.RData")
-plotData = cbind(summaryPara, blockData[blockData$blockNum == 1, ])
-ggplot(plotData, aes(tau, AUC)) + geom_point() + facet_grid(~condition)
--- subFxs/modelFittingFxs.R
+++ subFxs/modelFittingFxs.R
@@ -2,8 +2,8 @@
 modelFitting = function(cond, wIni, timeWaited, trialEarning, fileName, pars, model){
   tMax = ifelse(cond == "HP", tMaxs[1], tMaxs[2])
   condIdx = ifelse(cond =="HP", 1, 2)
-  nChain = 2
-  nIter = 5000
+  nChain = 3
+  nIter = 3000
   nTimeStep = tMax / stepDuration
   nTimePoints = round(ifelse(trialEarnings >0, ceiling(timeWaited / stepDuration), floor(timeWaited / stepDuration) + 1))
   data_list <- list(tMax = tMax,
@@ -18,14 +18,37 @@ modelFitting = function(cond, wIni, timeWaited, trialEarning, fileName, pars, mo
   fit = sampling(object = model, data = data_list, cores = nChain, chains = nChain,
                iter = nIter, 
                show_messages = F) 
-  tempt = fit %>%
-    rstan::extract(permuted = F, pars = c(pars, "LL_all")) %>%
+  # extract parameters
+  extractedPara = fit %>%
+    rstan::extract(permuted = F, pars = c(pars, "LL_all"))
+  # save sampling sequences
+  tempt = extractedPara %>%
     adply(2, function(x) x) %>%  # change arrays into 2-d dataframe 
     select(-chains) 
   write.csv(tempt, file = sprintf("%s.txt", fileName), row.names=FALSE)
+  # calculate and save WAIC
   log_lik = extract_log_lik(fit)
   WAIC = waic(log_lik)
   save("WAIC", file = sprintf("%s.RData", fileName))
+  # calculate potential scale reduction 
+  n = nInter / 2 # number of samples used in each seq
+  m = nChain
+grandMean = extractedPara %>% apply(FUN = mean, MARGIN = c(3))
+seqMean = extractedPara %>% apply(FUN = mean, MARGIN = c(2,3))
+seqVar = extractedPara %>% apply(FUN = var, MARGIN = c(2,3)) # sample variance with n-1 degree
+B = seqMean %>% apply(FUN = var, MARGIN = 2) * n
+W =  seqVar %>% apply(FUN = function(x) sum(x) / m, MARGIN = 2)
+sigmaSquare = (n-1) / n * W + B / n
+V = sigmaSquare + B/(nChain * n)
+covS2X2 = mapply(FUN = cov, lapply(1 : length(pars), function(i) seqVar[,i]),
+                 lapply(1 : length(pars), function(i) seqMean[,i]^2))
+covS2X = mapply(FUN = cov, lapply(1 : length(pars), function(i) seqVar[,i]),
+                 lapply(1 : length(pars), function(i) seqMean[,i]))
+VVar = ((n-1) / n) ^ 2 /  m * apply(seqVar, FUN = var, MARGIN = 2) +
+  ((m+1) / m / n)^2 *2 /(m - 1) * B ^ 2 +
+  2 * (m+1) * (n-1) / (m * n^2) * n / m * (covS2X2 - 2 * grandMean * covS2X)
+df =  2 * V^2 / var(V)
+R = V / W * df / (df - 2)
 }
 
 
